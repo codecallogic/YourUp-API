42c3ef07a5c75521e711a0911178f80e2e97cbca
Fabricio <contact@fabricioguardia.com>
2021-05-28 06:57:56 -0700

    cookie lax before spotify redirect

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,12 @@
+const Cookies = require('cookies')
+
 exports.login = (req, res) => {
   const {displayName, email, photoURL} = req.body
   const userClient = {displayName, email, photoURL}
   console.log(req.body)
+
   return res.status(202).cookie("user", JSON.stringify(userClient), {
-    sameSite: 'strict',
+    sameSite: 'Lax',
     expires: new Date(new Date().getTime() + (60 * 60 * 1000)),
     httpOnly: true
   })
